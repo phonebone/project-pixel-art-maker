32d714f87dad40df7734c7f96d41828bd66f94f1
Jake <rottenapplesinc@gmail.com>
2017-09-13 22:15:48 +0200

    style: change double quotes to single quotes for consistency

--- designs.js
+++ designs.js
@@ -1,25 +1,25 @@
-const form = document.getElementById("sizePicker");
+const form = document.getElementById('sizePicker');
 let canvas,
-    color = "#eb5c81",
+    color = '#eb5c81',
     grid = {
       width: 1,
       height:1
     };
 
 // Wait for page to load before initializing:
-window.addEventListener("load", init, false);
+window.addEventListener('load', init, false);
 
 // Initialize:
 function init(){
-  const colorPicker = document.getElementById("colorPicker");
-  canvas = document.getElementById("pixel_canvas");
+  const colorPicker = document.getElementById('colorPicker');
+  canvas = document.getElementById('pixel_canvas');
   colorPicker.value = color;
   // if you want/need to change the color value in real time
   // colorPicker.addEventListener("input", updateColor, false);
-  colorPicker.addEventListener("change", updateColor, false);
+  colorPicker.addEventListener('change', updateColor, false);
   // When size is submitted by the user, call makeGrid()
-  form.addEventListener("submit", makeGrid, false);
-  canvas.addEventListener("click", colorCell, false)
+  form.addEventListener('submit', makeGrid, false);
+  canvas.addEventListener('click', colorCell, false)
   // if the user's browser doesn't support a color well but instead shows a text
   // field for entering the color string directly, this selects the text in the
   // edit field, otherwise this does nothing.
@@ -44,7 +44,7 @@ function makeGrid(e) {
 
 // Set bg color of cell:
 function colorCell(e){
-  if(e.target && e.target.nodeName === "TD") {
+  if(e.target && e.target.nodeName === 'TD') {
     e.target.bgColor = color;
   }
 }
@@ -74,9 +74,9 @@ function newGrid(){
   // grid rows
   for(let r = 0; r<grid.height; r++){
     // grid columns
-    let row = document.createElement("tr");
+    let row = document.createElement('tr');
     for(let c = 0; c<grid.width; c++){
-      let cell = document.createElement("td");
+      let cell = document.createElement('td');
       row.appendChild(cell);
     }
     canvas.appendChild(row);
